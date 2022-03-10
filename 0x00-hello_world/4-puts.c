20a5c569d98ec3ebb1269fc0830b51354454ad56
gicheru <harrysonjohn4@gmail.com>
2022-03-10 08:10:16 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,17 @@
  * the program prints programming is like building a multilingual puzzle
  */
 #include <stdio.h>
+/**
+ * int main(void) - entry of my program
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+/**
+ * puts(string) - prints the words inside the brackets
+ */
+	puts("\"Programming is like building a multilingual puzzle");
+/**
+ * returns the value o
+ */
 	return (0);
 }
