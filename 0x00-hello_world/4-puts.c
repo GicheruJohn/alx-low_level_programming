3fcd56e1920d0d56b706f0f79f8504c42c72f0fd
gicheru <harrysonjohn4@gmail.com>
2022-03-10 07:47:15 -0800

    using puts to print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+/**
+ * the program prints programming is like building a multilingual puzzle
+ */
+#include <stdio.h>
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
