0de2a4f39bb75f6afb6ea6ddb2f88d14819d7054
gicheru <harrysonjohn4@gmail.com>
2022-03-16 21:36:27 +0000

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - computes absolute value of an integer
+ *
+ * @n: integer input
+ *
+ * Return: always 0
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		_putchar(n);
+	}
+	else
+	{
+		_putchar(n * -1);
+	}
+}
