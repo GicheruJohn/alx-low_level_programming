fa228cc29773e15de8b4270c3aa05373f96a4c5e
gicheru <harrysonjohn4@gmail.com>
2022-03-16 22:34:44 +0000

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of jack bauer
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int H, M;
+
+	H = 0;
+	if (H < 24)
+	{
+		M = 0;
+		if (M < 60)
+		{
+			_putchar(H":"M);
+		}
+		M++;
+	}
+	H++;
+}
