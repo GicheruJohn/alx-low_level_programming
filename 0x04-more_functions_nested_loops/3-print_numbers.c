af57da2f1d8cf9d3d992d7734cb8173dc056f048
gicheru <harrysonjohn4@gmail.com>
2022-03-18 11:42:54 -0700

    rv num

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,11 @@
  */
 void print_numbers(void)
 {
-	int num;
+	int x;
 
-	for (num = '0', num < '10', num++)
+	for (x = '0', x < '10', x++)
 	{
-		_putchar(num);
+		_putchar(x);
 	}
 	_putchar('\n');
 }
